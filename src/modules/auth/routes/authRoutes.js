bbb749a3a2c3eafd5c1590a9741f3f3ee57c4f3f
raul <raulcv7.mail@gmail.com>
2026-02-21 20:53:43 +0100

    Feat: añadir ruta de login con validaciones de entrada

--- src/modules/auth/routes/authRoutes.js
+++ src/modules/auth/routes/authRoutes.js
@@ -1,17 +1,26 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
-import { registerUser } from '../controllers/authController.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = Router();
 
 /**
  * Petición POST a /api/auth/register
- * Validaciones exhaustivas de entrada
+ * Registro de un nuevo usuario
  */
 router.post('/register', [
     body('username').notEmpty().withMessage('El nombre de usuario es obligatorio'),
     body('email').isEmail().withMessage('Debe ser un email válido'),
     body('contrasena').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres')
 ], registerUser);
+
+/**
+ * Petición POST a /api/auth/login
+ * Inicio de sesión de un usuario existente
+ */
+router.post('/login', [
+    body('email').isEmail().withMessage('Debe ser un email válido'),
+    body('contrasena').notEmpty().withMessage('La contraseña es obligatoria')
+], loginUser);
 
 export default router;
