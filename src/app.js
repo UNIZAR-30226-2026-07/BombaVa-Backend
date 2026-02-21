a19a61674727d501c263b894980072602a31f2ae
raul <raulcv7.mail@gmail.com>
2026-02-21 22:10:15 +0100

    IMP: registrar rutas para matches en el entrypoint de la app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@
 import cors from 'cors';
 import express from 'express';
 import authRoutes from './modules/auth/routes/authRoutes.js';
+import matchRoutes from './modules/game/routes/matchRoutes.js';
 import inventoryRoutes from './modules/inventory/routes/inventoryRoutes.js';
 import { errorHandler } from './shared/middlewares/errorMiddleware.js';
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/inventory', inventoryRoutes);
+app.use('/api/matches', matchRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: "TEST-API de BombaVa" });
