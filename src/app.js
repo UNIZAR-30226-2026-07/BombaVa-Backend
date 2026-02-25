9b89f2e68cb0efca527f8a319314d5f1827e6d74
OrcasAlbatross <orcasalbatross@gmail.com>
2026-02-25 17:33:20 +0100

    feat:  creada estructura API REST docs
    Creada la estructura inicial para poder empezar a documentar la api REST con swagger.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,18 @@
  */
 import cors from 'cors';
 import express from 'express';
+import YAML  from 'yamljs';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
 import { authRoutes } from './modules/auth/index.js';
 import { matchRoutes } from './modules/game/index.js';
 import { inventoryRoutes } from './modules/inventory/index.js';
 import { errorHandler } from './shared/middlewares/index.js';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi/openapi.yaml'));
+
 
 const app = express();
 
@@ -17,6 +25,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/inventory', inventoryRoutes);
 app.use('/api/matches', matchRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.json({ message: "API BombaVa V1 - HOME" });
